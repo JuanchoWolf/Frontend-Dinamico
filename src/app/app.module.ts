bcbe82770ad6722cae9e5985c8f90394e5153485
JuanchoWolf <estefano.chicco@gmail.com>
2022-10-13 13:09:04 -0300

    delete projects

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,11 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { ProjectsComponent } from './projects/projects.component';
 import { AboutComponent } from './about/about.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProjectsComponent,
     AboutComponent
   ],
   imports: [
