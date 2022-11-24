9cd929a473a22820f331fb648aa2523ce7fd7af2
JuanchoWolf <estefano.chicco@gmail.com>
2022-11-24 08:12:28 -0300

    add

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { AboutComponent } from './about/about.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { DesplegableComponent } from './desplegable/desplegable.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AboutComponent
+    AboutComponent,
+    ProjectsComponent,
+    DesplegableComponent
   ],
   imports: [
     BrowserModule
